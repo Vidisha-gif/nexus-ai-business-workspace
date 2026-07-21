157e0a80992b120972f698f29e32205d49dfca72
vidishaverma89 <139036899+vidishaverma89@users.noreply.github.com>
2026-07-21 23:28:25 +0530

    Implement authentication module foundation

---
+++ backend/src/main/java/com/nexusai/backend/service/AuthService.java
@@ -0,0 +1,33 @@
+package com.nexusai.backend.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.nexusai.backend.entity.Role;
+import com.nexusai.backend.entity.User;
+import com.nexusai.backend.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User register(User user) {
+
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        user.setPassword(
+                passwordEncoder.encode(user.getPassword())
+        );
+
+        user.setRole(Role.EMPLOYEE);
+
+        return userRepository.save(user);
+    }
+}
